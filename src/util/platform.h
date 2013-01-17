6c07928a5e29a26ebca21ca55ea8f4c9aae57d3f
Michael Lippautz <michael.lippautz@gmail.com>
2013-01-17 14:53:42 +0100

    platform: Add func that gets number of cores (virtual + physical).
    Signed-off-by: Michael Lippautz <michael.lippautz@gmail.com>

--- src/util/platform.h
+++ src/util/platform.h
@@ -6,12 +6,19 @@
 #define SCAL_UTIL_PLATFORM_H_
 
 #include <stdint.h>
+#include <unistd.h>
 
 namespace scal {
 
 const uint64_t kPageSize = 4096;
 const uint64_t kCachelineSize = 64;
 const uint64_t kCachePrefetch = 128;
+
+// For sake of simplicity: Assume posix with optional _SC_NPROCESSORS_ONLN
+// variable.
+inline long number_of_cores() {
+  return sysconf(_SC_NPROCESSORS_ONLN);
+}
 
 }  // namespace scal
 
