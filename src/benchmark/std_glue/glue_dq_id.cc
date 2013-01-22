d08606606dd9134bfb1b705a1cec741119ec821f
Michael Lippautz <michael.lippautz@gmail.com>
2013-01-22 16:50:35 +0100

    Add thread id based balancer to framework.
    Signed-off-by: Michael Lippautz <michael.lippautz@gmail.com>

---
+++ src/benchmark/std_glue/glue_dq_id.cc
@@ -0,0 +1,39 @@
+// Copyright (c) 2012-2013, the Scal Project Authors.  All rights reserved.
+// Please see the AUTHORS file for details.  Use of this source code is governed
+// by a BSD license that can be found in the LICENSE file.
+
+#define __STDC_FORMAT_MACROS 1  // we want PRIu64 and friends
+
+#include <gflags/gflags.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <new>
+
+#include "benchmark/std_glue/std_pipe_api.h"
+#include "datastructures/balancer_id.h"
+#include "datastructures/distributed_queue.h"
+#include "util/malloc.h"
+
+DEFINE_uint64(p, 80, "number of partial queues");
+
+void* ds_new(void) {
+  BalancerId *balancer = new BalancerId();
+  DistributedQueue<uint64_t> *dq = 
+      new DistributedQueue<uint64_t>(FLAGS_p, g_num_threads + 1, balancer);
+  return static_cast<void*>(dq);
+}
+
+bool ds_put(void *ds, uint64_t item) {
+  DistributedQueue<uint64_t> *dq = static_cast<DistributedQueue<uint64_t>*>(ds);
+  return dq->put(item);
+}
+
+bool ds_get(void *ds, uint64_t *item) {
+  DistributedQueue<uint64_t> *dq = static_cast<DistributedQueue<uint64_t>*>(ds);
+  return dq->get(item);
+}
+
+char* ds_get_stats(void) {
+  return NULL;
+}
---
+++ src/datastructures/balancer_id.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2012-2013, the Scal Project Authors.  All rights reserved.
+// Please see the AUTHORS file for details.  Use of this source code is governed
+// by a BSD license that can be found in the LICENSE file.
+
+#ifndef SCAL_DATASTRUCTURES_BALANCER_ID_H_
+#define SCAL_DATASTRUCTURES_BALANCER_ID_H_
+
+#include "datastructures/balancer.h"
+#include "util/threadlocals.h"
+
+class BalancerId : public BalancerInterface {
+ public:
+  BalancerId() { }
+
+  uint64_t get(uint64_t num_queues, MSQueue<uint64_t> **queues, bool enqueue) {
+    if (num_queues == 1) {
+      return 0;
+    }
+    uint64_t thread_id = threadlocals_get()->thread_id;
+    return thread_id % num_queues;
+  }
+};
+
+#endif  // SCAL_DATASTRUCTURES_BALANCER_ID_H_
