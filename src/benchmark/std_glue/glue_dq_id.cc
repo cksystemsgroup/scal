44da8a16c28429883efc0496556db1c82cc4ee3b
Michael Lippautz <michael.lippautz@gmail.com>
2013-01-24 15:45:35 +0100

    ID DQ: Adjust to new backend syntax.
    Signed-off-by: Michael Lippautz <michael.lippautz@gmail.com>

--- src/benchmark/std_glue/glue_dq_id.cc
+++ src/benchmark/std_glue/glue_dq_id.cc
@@ -13,24 +13,28 @@
 #include "benchmark/std_glue/std_pipe_api.h"
 #include "datastructures/balancer_id.h"
 #include "datastructures/distributed_queue.h"
+#include "datastructures/ms_queue.h"
 #include "util/malloc.h"
 
 DEFINE_uint64(p, 80, "number of partial queues");
 
 void* ds_new(void) {
   BalancerId *balancer = new BalancerId();
-  DistributedQueue<uint64_t> *dq = 
-      new DistributedQueue<uint64_t>(FLAGS_p, g_num_threads + 1, balancer);
+  DistributedQueue<uint64_t, MSQueue<uint64_t> > *dq =
+      new DistributedQueue<uint64_t, MSQueue<uint64_t> >(
+          FLAGS_p, g_num_threads + 1, balancer);
   return static_cast<void*>(dq);
 }
 
 bool ds_put(void *ds, uint64_t item) {
-  DistributedQueue<uint64_t> *dq = static_cast<DistributedQueue<uint64_t>*>(ds);
+  DistributedQueue<uint64_t, MSQueue<uint64_t> > *dq =
+      static_cast<DistributedQueue<uint64_t, MSQueue<uint64_t> >*>(ds);
   return dq->put(item);
 }
 
 bool ds_get(void *ds, uint64_t *item) {
-  DistributedQueue<uint64_t> *dq = static_cast<DistributedQueue<uint64_t>*>(ds);
+  DistributedQueue<uint64_t, MSQueue<uint64_t> > *dq =
+      static_cast<DistributedQueue<uint64_t, MSQueue<uint64_t> >*>(ds);
   return dq->get(item);
 }
 
