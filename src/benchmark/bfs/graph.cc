f02e662a23ead096b18eb1bbc906f96ed79b3e98
Michael Lippautz <michael.lippautz@gmail.com>
2013-01-25 16:02:33 +0100

    BFS: Cleanup graph abstraction.
    Signed-off-by: Michael Lippautz <michael.lippautz@gmail.com>

--- src/benchmark/bfs/graph.cc
+++ src/benchmark/bfs/graph.cc
@@ -1,9 +1,15 @@
+// Copyright (c) 2012-2013, the Scal Project Authors.  All rights reserved.
+// Please see the AUTHORS file for details.  Use of this source code is governed
+// by a BSD license that can be found in the LICENSE file.
+
 #include "benchmark/bfs/graph.h"
 
+#include <stdlib.h>
+
+#include <algorithm>
 #include <cstddef>
 #include <cstdlib>
-#include <cstring>
-#include <fstream>
+#include <string>
 #include <vector>
 
 #include "util/malloc.h"
@@ -11,7 +17,7 @@
 namespace {
 
 void invalid_graph_file(const char *fn) {
-  fprintf(stderr, "%s: invalid graph file", fn);
+  fprintf(stderr, "%s: invalid graph file\n", fn);
   exit(EXIT_FAILURE);
 }
 
@@ -22,27 +28,33 @@ bool inline is_comment(const std::string &source) {
   return false;
 }
 
-std::vector<uint64_t> inline tokenize(
-    const std::string &source, const char *delimiter = " ", bool keep_empty = false) {
+std::vector<uint64_t> inline tokenize(const std::string &source,
+                                      const char *delimiter = " ",
+                                      bool keep_empty = false) {
   std::vector<uint64_t> results;
   size_t prev = 0;
   size_t next = 0;
+  uint64_t num;
   while ((next = source.find_first_of(delimiter, prev)) != std::string::npos) {
     if (keep_empty || (next - prev != 0)) {
-      uint64_t num = atol(source.substr(prev, next - prev).c_str());
+      num = atol(source.substr(prev, next - prev).c_str());
       if (num == 0) {
+        printf("1asdfasf\n");
         invalid_graph_file(__func__);
       }
-      results.push_back(num - 1);  // we are zero-based (contrary to graph format)
+      // We use zero based indices and node ids.
+      results.push_back(num - 1);
     }
     prev = next + 1;
   }
   if (prev < source.size()) {
-    uint64_t num = atol(source.substr(prev).c_str());
+    num = atol(source.substr(prev).c_str());
     if (num == 0) {
+        printf("2asdfasf\n");
       invalid_graph_file(__func__);
     }
-    results.push_back(num - 1);  // we are zero-based (contrary to graph format)
+    // Again, we use zero based indices and node ids.
+    results.push_back(num - 1);
   }
   return results;
 }
@@ -51,11 +63,20 @@ std::vector<uint64_t> inline tokenize(
 
 Graph* Graph::from_graph_file(const char* graph_file) {
   Graph *g = new Graph();
-  std::ifstream infile(graph_file);
-  std::string line;
+  FILE* infile;
+  char line_buffer[BUFSIZ];
+
+  infile = fopen(graph_file, "rt");
+  if (!infile) {
+    fprintf(stderr, "%s: unable to open file %s\n", __func__, graph_file);
+    exit(EXIT_FAILURE);
+  }
+
   bool firstline = true;
   uint64_t id = 0;
-  while(std::getline(infile, line)) {
+  while (fgets(line_buffer, sizeof(line_buffer), infile)) {
+    std::string line(line_buffer);
+    line.erase(std::remove(line.begin(), line.end(), '\n'), line.end());
     if (is_comment(line)) {
       continue;
     }
@@ -72,14 +93,15 @@ Graph* Graph::from_graph_file(const char* graph_file) {
       g->num_vertices_ = tokens[0];
       for (uint64_t i = 0; i < tokens[0]; i++) {
         g->vertices_[i] = scal::get<Vertex>(4 * 128);
-        g->vertices_[i]->distance = Vertex::distance_not_set;
+        g->vertices_[i]->distance = Vertex::no_distance;
       }
       continue;
     } 
     std::vector<uint64_t> tokens = tokenize(line, " ");
     Vertex *v = g->vertices_[id];
     if (tokens.size() > 0) {
-      v->neighbors = static_cast<uint64_t*>(scal::calloc_aligned(tokens.size(), sizeof(uint64_t), 128));
+      v->neighbors = static_cast<uint64_t*>(scal::calloc_aligned(
+          tokens.size(), sizeof(uint64_t), 128));
       v->len_neighbors = tokens.size();
       for (uint64_t i = 0; i < tokens.size(); i++) {
         v->neighbors[i] = tokens[i];
@@ -90,5 +112,8 @@ Graph* Graph::from_graph_file(const char* graph_file) {
     }
     id++;
   }
+
+  fclose(infile);
+
   return g;
 }
--- src/benchmark/bfs/graph.h
+++ src/benchmark/bfs/graph.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2012-2013, the Scal Project Authors.  All rights reserved.
+// Please see the AUTHORS file for details.  Use of this source code is governed
+// by a BSD license that can be found in the LICENSE file.
+
 #ifndef SCAL_BENCHMARK_BFS_GRAPH_H
 #define SCAL_BENCHMARK_BFS_GRAPH_H
 
@@ -6,7 +10,7 @@
 #include <limits>
 
 struct Vertex {
-  static const uint64_t distance_not_set = std::numeric_limits<uint64_t>::max();
+  static const uint64_t no_distance = std::numeric_limits<uint64_t>::max();
 
   uint64_t *neighbors;
   uint64_t len_neighbors;
@@ -17,7 +21,7 @@ class Graph {
  public:
   static Graph* from_graph_file(const char* graph_file);
   
-  inline uint64_t num_vertices() {
+  inline uint64_t size() {
     return num_vertices_;
   }
 
@@ -26,8 +30,8 @@ class Graph {
   }
 
  private:
-  Graph() { };
-  Graph(Graph& cpy) { };
+  Graph() {}
+  Graph(const Graph &cpy) {}
 
   uint64_t num_vertices_;
   Vertex **vertices_;
