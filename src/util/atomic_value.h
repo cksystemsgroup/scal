5392556f014e66fff8dd87b88da7870e8842eb3f
Michael Lippautz <michael.lippautz@gmail.com>
2013-01-27 19:13:14 +0100

    Remove dependency on template aliasing support.
    If template aliasing is not supported, macros are used.
    
    Signed-off-by: Michael Lippautz <michael.lippautz@gmail.com>

--- src/util/atomic_value.h
+++ src/util/atomic_value.h
@@ -5,23 +5,38 @@
 #ifndef SCAL_UTIL_ATOMIC_VALUE_H_
 #define SCAL_UTIL_ATOMIC_VALUE_H_
 
-
 // If we don't have a config.h file, just use the 128bit CAS (default).
 #ifdef HAVE_CONFIG_H
 #include "./config.h"
 #else
 #define USE_CAS128
-#endif  //  HAVE_CONFIG_H
+#endif  // HAVE_CONFIG_H
 
 #ifdef USE_CAS128
+
 #include "util/atomic_value128.h"
+
+#ifdef SUPPORTS_TEMPLATE_ALIAS
 template<typename T> using AtomicPointer = AtomicValue128<T>;
 template<typename T> using AtomicValue = AtomicValue128<T>;
 #else
+#define AtomicPointer AtomicValue128
+#define AtomicValue AtomicValue128
+#endif  // SUPPORTS_TEMPLATE_ALIAS
+
+#else
+
 #include "util/atomic_value64_offset.h"
 #include "util/atomic_value64_no_offset.h"
+
+#ifdef SUPPORTS_TEMPLATE_ALIAS
 template<typename T> using AtomicPointer = AtomicValue64Offset<T>;
 template<typename T> using AtomicValue = AtomicValue64NoOffset<T>;
+#else
+#define AtomicPointer AtomicValue64Offset
+#define AtomicValue AtomicValue64NoOffset
+#endif  // SUPPORTS_TEMPLATE_ALIAS
+
 #endif  // USE_CAS128
 
 #endif  // SCAL_UTIL_ATOMIC_VALUE_H_
