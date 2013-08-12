9a0eb350947aff9b1367836b2110b1b48638f967
Andreas Haas <dra.haas@gmail.com>
2013-08-12 09:13:19 +0200

    adjusted benchmark output

--- src/benchmark/seqalt/seqalt.cc
+++ src/benchmark/seqalt/seqalt.cc
@@ -86,7 +86,7 @@ int main(int argc, const char **argv) {
   if (FLAGS_print_summary) {
     uint64_t exec_time = benchmark->execution_time();
     char buffer[1024] = {0};
-    uint32_t n = snprintf(buffer, sizeof(buffer), "threads: %" PRIu64 " ;runtime: %" PRIu64 " ;operations: %" PRIu64 " ;c: %" PRIu64 " ;aggr: %" PRIu64 ";ds_stats: ",
+    uint32_t n = snprintf(buffer, sizeof(buffer), "threads: %" PRIu64 " ;runtime: %" PRIu64 " ;operations: %" PRIu64 " ;c: %" PRIu64 " ;aggr: %" PRIu64 " ;ds_stats: ",
         FLAGS_threads,
         exec_time,
         FLAGS_elements,
