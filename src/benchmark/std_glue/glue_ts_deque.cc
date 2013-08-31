e465cd956e635ba9b9d14fe3d73e21ae6fff008e
Andreas Haas <dra.haas@gmail.com>
2013-08-31 15:02:11 +0200

    bugfix

--- src/benchmark/std_glue/glue_ts_deque.cc
+++ src/benchmark/std_glue/glue_ts_deque.cc
@@ -2,8 +2,9 @@
 // Please see the AUTHORS file for details.  Use of this source code is governed
 // by a BSD license that can be found in the LICENSE file.
 
-#include <gflags/gflags.h>
+#define __STDC_FORMAT_MACROS 1  // we want PRIu64 and friends
 
+#include <gflags/gflags.h>
 #include "benchmark/std_glue/std_pipe_api.h"
 #include "datastructures/ts_timestamp.h"
 #include "datastructures/ts_deque.h"
@@ -17,6 +18,8 @@ DEFINE_bool(init_threshold, false, "initializes the dequeue threshold "
     "with the current time");
 DEFINE_int64(delay, -1, "delay in the insert operation");
 
+uint64_t g_delay;
+
 void* ds_new() {
   TimeStamp *timestamping;
   if (FLAGS_stutter_clock) {
@@ -29,14 +32,13 @@ void* ds_new() {
     timestamping = new ShiftedHardwareTimeStamp();
   }
 
-  uint64_t delay;
   if (FLAGS_delay >= 0) {
-    delay = FLAGS_delay;
+    g_delay = FLAGS_delay;
   } else {
     if (g_num_threads <= 2) {
-      delay = 0;
+      g_delay = 0;
     } else {
-      delay = g_num_threads * 60;
+      g_delay = g_num_threads * 60;
     }
   }
 
@@ -45,7 +47,7 @@ void* ds_new() {
     buffer = new TLLinkedListDequeBuffer<uint64_t>(g_num_threads + 1);
   } else if (FLAGS_2ts) {
     buffer 
-      = new TL2TSDequeBuffer<uint64_t>(g_num_threads + 1, FLAGS_delay);
+      = new TL2TSDequeBuffer<uint64_t>(g_num_threads + 1, g_delay);
   }
   TSDeque<uint64_t> *ts =
       new TSDeque<uint64_t>(buffer, timestamping, FLAGS_init_threshold);
@@ -53,5 +55,16 @@ void* ds_new() {
 }
 
 char* ds_get_stats(void) {
-  return NULL;
+  char buffer[255] = { 0 };
+  uint32_t n = snprintf(buffer,
+                        sizeof(buffer),
+                        "%" PRIu64 "",
+                        g_delay);
+  if (n != strlen(buffer)) {
+    fprintf(stderr, "%s: error creating stats string\n", __func__);
+    abort();
+  }
+  char *newbuf = static_cast<char*>(calloc(
+      strlen(buffer) + 1, sizeof(*newbuf)));
+  return strncpy(newbuf, buffer, strlen(buffer));
 }
