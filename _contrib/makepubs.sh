52e8e6b9d5f4201ae11b8b9c5654692323912364
Michael Lippautz <michael.lippautz@gmail.com>
2015-01-08 11:27:53 +0100

    Remove "fork us" ribbon (buggy in chrome) and add POPL artifact stamp.

--- _contrib/makepubs.sh
+++ _contrib/makepubs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Call from base directory:
+#   _contrib/makepubs.sh
+
+
 TMPDIR=. bibtex2html -nobibsource -noheader -nofooter -nodoc -unicode -dl -nokeys -d -r scal.bib
 
 cp scal.bib scal.bib.txt
